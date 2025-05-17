dff5637e187a15658500ed8b8cb4bbb2a6205505
João <Sem e-mail>
2025-05-17 20:17:01 -0300

    Iniciando com lógica de reprodução de população

--- src/main/java/com/genetico/model/Populacao.java
+++ src/main/java/com/genetico/model/Populacao.java
@@ -3,6 +3,7 @@ package com.genetico.model;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 public class Populacao {
     private List<Cromossomo> cromossomos;
@@ -38,6 +39,24 @@ public class Populacao {
 
     public void imprimirPopulacao() {
         for (Cromossomo cromossomo : cromossomos) System.out.println(cromossomo.imprimirGenes());
+    }
+
+    public void reproduzir() {
+        var random = new Random();
+
+        var indiceAleatorio1 = random.nextInt(TAMANHO_POPULACAO);
+        var indiceAleatorio2 = random.nextInt(TAMANHO_POPULACAO);
+
+        var pai1 = cromossomos.get(indiceAleatorio1);
+        var pai2 = cromossomos.get(indiceAleatorio2);
+
+        var filhos = pai1.realizarCrossoverPmx(pai2, new Random());
+
+        var filho1 = filhos.getFirst();
+        var filho2 = filhos.getLast();
+
+        System.out.println(filho1.imprimirGenes());
+        System.out.println(filho2.imprimirGenes());
     }
 
     public List<Cromossomo> getCromossomos() {
